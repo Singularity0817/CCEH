6c239e27548d62006e2df0ac75ef73fd5c014595
Wenhui Zhang <Singularity_x@outlook.com>
2021-01-12 17:34:47 -0600

    modify the way to record runtime put throughput

--- log-structured-db-cuckoo.cpp
+++ log-structured-db-cuckoo.cpp
@@ -35,10 +35,11 @@ const size_t InsertSize = 1000*1024*1024;
 const size_t recordInterval = 1024*1024;
 //const int BatchSize = 1024;
 const int ServerNum = 8;
+const unsigned GetThreadNum = 16;
 const size_t InsertSizePerServer = InsertSize/ServerNum;
 const Value_t ConstValue[2] = {"VALUE_1", "value_2"};
 const size_t testTimes = 1;
-const size_t valueSize = 16;
+const size_t valueSize = 8;
 const size_t LogEntrySize = sizeof(Key_t)+sizeof(size_t)+valueSize;
 const size_t EstimateLogSize = testTimes*(512+(LogEntrySize*InsertSizePerServer)+1024);
 //const size_t EstimateLogSize = 512*1024*1024;
@@ -47,6 +48,9 @@ const size_t EstimateLogSize = testTimes*(512+(LogEntrySize*InsertSizePerServer)
 #define RESERVE_MODE
 //#define RECORD_WA
 #define RECORD_AS_PROGRESS
+
+#define RECORD_GET_LAT
+#define RECORD_PUT_LAT
 
 inline uint64_t GetTimeNsec()
 {
@@ -275,20 +279,26 @@ struct db_server_param
     std::atomic<int> *readyCount;
     size_t finishSize;
     double ops;
+    vector<double> put_ops;
+#ifdef RECORD_PUT_LAT
+    vector<unsigned> put_lats;
+#endif
     db_server_param(int _id, myDB* _db, std::atomic<bool> *_start, std::atomic<int> *_readyCount)
-        : id(_id), db(_db), start(_start), readyCount(_readyCount), finishSize(0) {}
+        : id(_id), db(_db), start(_start), readyCount(_readyCount), finishSize(0) {
+#ifdef RECORD_PUT_LAT
+        put_lats = vector<unsigned>(1000, 0);
+#endif
+        put_ops = vector<double>(1001, 0.0);
+        }
 };
 
 void db_server(db_server_param *p)
 {
-    //PinCore("server");
     myDB *db = p->db;
     int id = p->id;
-    //size_t *finishSize = p->finishSize;
-    //vector<Pair> pairs_to_put;
-    //Pair kv_pair;
     Key_t key;
     unsigned counter = 0;
+    unsigned last_counter = 0;
     std::vector<unsigned> keys;
     for (unsigned i = 0; i < InsertSizePerServer; i++) {
         keys.push_back(i*ServerNum+id);
@@ -296,34 +306,91 @@ void db_server(db_server_param *p)
     char value[valueSize];
     memset(value, 'a', valueSize-1);
     value[valueSize-1] = '\0';
+#ifdef RECORD_PUT_LAT
+    uint64_t put_begin, put_span;
+#endif
     cout_lock.lock();
     cout << "Server " << id << " shuffling keys." << endl;
     cout_lock.unlock();
     std::random_shuffle(keys.begin(), keys.end());
-    //__sync_fetch_and_add(p->readyCount, 1);
-    p->readyCount->fetch_add(1);
+    unsigned progress = 0;
     cout_lock.lock();
     cout << "Server " << id << " is ready with db " << db << "." << endl;
     cout_lock.unlock();
+    p->readyCount->fetch_add(1);
     while(!(p->start->load(std::memory_order_relaxed))) {}
     uint64_t thread_start = GetTimeNsec();
+    uint64_t last_checkpoint = thread_start;
+    uint64_t new_checkpoint;
     for (unsigned t = 0; t < testTimes; t++) {
-        //for (unsigned i = 0; i < InsertSizePerServer; i++) {
         for (auto it = keys.begin(); it != keys.end(); it++) {
             //key = i*ServerNum+id;
             key = *it;
+#ifdef RECORD_PUT_LAT
+            put_begin = GetTimeNsec();
+#endif
             db->Insert(key, valueSize, value);//ConstValue[0]);
+#ifdef RECORD_PUT_LAT
+            put_span = GetTimeNsec()-put_begin;
+            if (put_span/10 < 1000) ++p->put_lats[put_span/10];
+            else ++p->put_lats[999];
+#endif
             counter++;
-            if (counter == 1000) {
-                p->finishSize += counter;
-                counter = 0;
+            if (counter*1000.0/InsertSizePerServer >= progress+1) {
+                new_checkpoint = GetTimeNsec();
+                p->put_ops[progress] = (counter-last_counter)*1000000000.0/(new_checkpoint-last_checkpoint);
+                //fprintf(stderr, "\rprogress %2.1f%%, ops %.0lf", progress/10.0, p->put_ops[progress]);
+                last_checkpoint = new_checkpoint;
+                last_counter = counter;
+                progress++;
             }
         }
     }
-    p->finishSize += counter;
-    counter = 0;
     p->ops = (InsertSizePerServer * testTimes) / ((GetTimeNsec() - thread_start) / 1000000000.0);
-    //db->print_put_stat();
+}
+
+struct db_get_thread_param
+{
+    myDB **dbs;
+    unsigned item_to_get;
+    uint64_t key_range;
+    std::atomic<bool> *start;
+    std::atomic<unsigned> *ready_count;
+    unsigned failed_get = 0;
+    std::vector<unsigned> get_lats;
+    db_get_thread_param(myDB **_dbs, unsigned _item, uint64_t _range, std::atomic<bool> *_s,
+                     std::atomic<unsigned> *_rc) : 
+                     dbs(_dbs), item_to_get(_item), key_range(_range), start(_s), ready_count(_rc) {
+        get_lats = std::vector<unsigned>(1000, 0);
+    }
+};
+
+void GetTestThread(db_get_thread_param *p) {
+    std::vector<uint64_t> keys;
+    char value[valueSize];
+    default_random_engine re(time(0));
+    uniform_int_distribution<uint64_t> u(0, p->key_range-1);
+    uint64_t tstart, tspan;
+    //prepare random keys to get
+    for (unsigned i = 0; i < p->item_to_get; ++i) {
+        keys.push_back(u(re));
+    }
+    unsigned db_mask = ServerNum-1;
+    p->ready_count->fetch_add(1, std::memory_order_acq_rel);
+    while(!p->start->load(std::memory_order_acquire)) {asm("nop");}
+    for (unsigned i = 0; i < p->item_to_get; ++i) {
+#ifdef RECORD_GET_LAT
+        tstart = GetTimeNsec();
+#endif
+        if (!(p->dbs[keys[i]&db_mask]->Get(keys[i], value))) {
+            p->failed_get++;
+        }
+#ifdef RECORD_GET_LAT
+        tspan = GetTimeNsec()-tstart;
+        if (tspan/10 < 1000) ++p->get_lats[tspan/10];
+        else ++p->get_lats[999];
+#endif
+    }
 }
 
 struct db_open_param
@@ -408,109 +475,79 @@ int main(int argc, char *argv[]){
 #ifdef RECORD_WA
         IPMWatcher write_watcher("write");
 #endif
-        uint64_t old_progress_checkpoint, new_progress_checkpoint;
-        size_t new_fs, old_fs = 0;
-        clock_gettime(CLOCK_REALTIME, &time_start);
-        old_progress_checkpoint = time_start.tv_sec*1000000000+time_start.tv_nsec;
-        uint64_t put_start_time = old_progress_checkpoint;
-        start = true;
-        double wa = 0;
-        printf("runtime    progress    ops    wa    avg_ops\n");
-        while (finishSize < InsertSize) {
-            finishSize = 0;
-            for (int i = 0; i < ServerNum; i++) {
-                finishSize += dbParams[i]->finishSize;
-            }
-            new_fs = finishSize;
-            new_progress = new_fs*100.0/(double)InsertSize;
-
-            new_progress_checkpoint = GetTimeNsec();
-#ifndef RECORD_AS_PROGRESS            
-            if (new_progress_checkpoint - old_progress_checkpoint >= 1000000000) {
-#else
-            //if (new_progress - old_progress >= 0.1) {
-            if (new_fs - old_fs >= recordInterval) {
-#endif
-                //new_progress_checkpoint = GetTimeNsec();
-                //double span = (time_middle.tv_sec - time_start.tv_sec) + (time_middle.tv_nsec - time_start.tv_nsec)/1000000000.0;
-                printf("%.1lf    %2.1lf%%    %.1lf    %.2lf    %.1lf\n", 
-                    (new_progress_checkpoint - put_start_time)/1000000000.0,
-                    new_progress, (new_fs-old_fs)/(double)((new_progress_checkpoint-old_progress_checkpoint)/1000000000.0),
-#ifdef RECORD_WA
-                    (double) write_watcher.CheckDataWriteToDIMM()/(new_fs*16.0),
-#else
-                    wa,
-#endif
-                    new_fs/(double)((new_progress_checkpoint-put_start_time)/1000000000.0));
-                //write_watcher.Checkpoint();
-                fflush(stdout);
-                old_progress = new_progress;
-                old_progress_checkpoint = new_progress_checkpoint;
-                old_fs = new_fs;
-            }
-        }
-        printf("\n");
-        clock_gettime(CLOCK_REALTIME, &time_end);
-        }
+        start.store(true, std::memory_order_release);
         for (int i = 0; i < ServerNum; i++) {
             server_threads[i].join();
         }
-        time_span = ((time_end.tv_sec - time_start.tv_sec) + (time_end.tv_nsec - time_start.tv_nsec)/1000000000.0);
-        //double ops = InsertSize/(double)time_span;
+        vector<double> put_ops(1000, 0.0);
+        for (int i = 0; i < ServerNum; ++i) {
+            for (unsigned j = 0; j < 1000; ++j) {
+                put_ops[j] += dbParams[i]->put_ops[j];
+            }
+        }
+        printf("progress    ops\n");
+        for (unsigned i = 0; i < 1000; ++i) {
+            printf("   %3.1lf%%  %.0lf\n", i/10.0, put_ops[i]);
+        }
         double ops = 0;
         for (int i = 0; i < ServerNum; i++) {
             ops += (dbParams[i]->ops);
         }
         std::cout << "Insert ops: " << ops << std::endl;
-
-        //zExecute(mem_command);
-        fflush(stdout);
-        int failSearch = 0;
-        std::default_random_engine re(time(0));
-        //std::uniform_int_distribution<Key_t> u(0, InsertSize-1);
-        std::uniform_int_distribution<Key_t> u(InsertSize, 10*InsertSize);
-        uint64_t get_time_span = 0;
-        uint64_t get_time_max = 0, get_time_min = ~0, get_time_this;
-        unsigned itemstoget = 1000000;
-        uint64_t rtime[1000];
-        for (int i = 0; i < 1000; i++) rtime[i] = 0;
-        unsigned wrongget = 0;
-        unsigned failedget = 0;
-        Key_t key;
-        //Value_t value;
-        char value[valueSize];
-        {
-        //IPMWatcher write_watcher("read");
-        for(unsigned i=0; i<itemstoget; i++){
-            key = u(re);
-            clock_gettime(CLOCK_REALTIME, &time_start);
-            auto ret = dbs[key%ServerNum]->Get(key, value);
-            clock_gettime(CLOCK_REALTIME, &time_end);
-            get_time_this = ((time_end.tv_sec - time_start.tv_sec)*1000000000 + (time_end.tv_nsec - time_start.tv_nsec));
-            //cout << "Value for key " << key << " is " << ret << endl;
-            if (ret == 0) {
-                failedget++;
-            //} else if (strcmp(value, ConstValue[((key-key%ServerNum)/ServerNum)%2]) != 0) {
-            }/* else if (strcmp(value, ConstValue[0]) != 0) {
-                wrongget++;
-                //cout << "Wrong value for key " << key << " : " << ret << endl;
-            }*/
-            get_time_span += get_time_this;
-            if (get_time_this > get_time_max) get_time_max = get_time_this;
-            if (get_time_this < get_time_min) get_time_min = get_time_this;
-            if (get_time_this >= 10000) {
-                rtime[999]++;
-            } else {
-                rtime[get_time_this/10]++;
+#ifdef RECORD_PUT_LAT
+        vector<unsigned> wtime = vector<unsigned>(1000, 0);
+        for(unsigned i = 0; i < ServerNum; i++){
+            for (unsigned j = 0; j < 1000; ++j) {
+                wtime[j] += dbParams[i]->put_lats[j];
             }
         }
-        std::cout << "Avg Get Lat: " << get_time_span/(double)itemstoget << "ns, max " << get_time_max << "ns, min " << get_time_min << "ns." << std::endl;
-        cout << "Wrong get num " << wrongget << endl
-            << "Fail get num " << failedget << endl;
+        std::cout << "Put time CDF." << std::endl;
+        for (int i = 0; i < 1000; i++) {
+            printf("%d %llu\n", i*10, wtime[i]);
+        }
+#endif
+        }
+        std::cout << "Begin get test with " << GetThreadNum << " threads" << std::endl;
+        //zExecute(mem_command);
+        {
+        fflush(stdout);
+        unsigned itemstoget = (unsigned)100*1024*1024;
+        unsigned itemsperthread = itemstoget/GetThreadNum;
+#ifdef RECORD_GET_LAT
+        vector<unsigned> rtime = vector<unsigned>(1000, 0);
+#endif
+        atomic<bool> get_start(false);
+        atomic<unsigned> get_thread_ready_count(0);
+        db_get_thread_param *get_params[GetThreadNum];
+        thread get_threads[GetThreadNum];
+        for (unsigned i = 0; i < GetThreadNum; ++i) {
+            get_params[i] = new db_get_thread_param(dbs,
+                                                 itemsperthread,
+                                                 InsertSize,
+                                                 &get_start,
+                                                 &get_thread_ready_count);
+            get_threads[i] = thread(GetTestThread, get_params[i]);
+        }
+        while (get_thread_ready_count.load(std::memory_order_acquire) < GetThreadNum) {asm("nop");}
+        get_start.store(true, std::memory_order_release);
+        uint64_t tstart = GetTimeNsec();
+        for (unsigned i = 0; i < GetThreadNum; ++i) {
+            get_threads[i].join();
+        }
+        uint64_t tspan = GetTimeNsec() - tstart;
+        std::cout << "Get test time span:   " << tspan << "ns" << std::endl
+                  << "Get throughput:       " << itemstoget/(tspan/1000000000.0) << "Ops/s" << std::endl;
+#ifdef RECORD_GET_LAT
+        for(unsigned i = 0; i < GetThreadNum; i++){
+            for (unsigned j = 0; j < 1000; ++j) {
+                rtime[j] += get_params[i]->get_lats[j];
+            }
+        }
         std::cout << "Get time CDF." << std::endl;
         for (int i = 0; i < 1000; i++) {
             printf("%d %llu\n", i*10, rtime[i]);
         }
+#endif
         }
         //zExecute(mem_command);
         fflush(stdout);
